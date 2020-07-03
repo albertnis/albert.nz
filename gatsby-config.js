dfd97154a83c60d86550e8d9ab442da20bbc4e88
Albert Nisbet <albertnis@gmail.com>
2020-07-04 10:50:19 +1200

    Get RSS feed working

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,67 @@ module.exports = {
         trackingId: `UA-45382817-3`,
       },
     },
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) =>
+              allMarkdownRemark.edges.map(edge =>
+                Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [
+                    {
+                      'content:encoded': `${
+                        edge.node.frontmatter.links
+                          ? `<p>${edge.node.frontmatter.links.join(' | ')}</p>`
+                          : ''
+                      }${edge.node.html}`,
+                    },
+                  ],
+                })
+              ),
+            query: `
+            {
+              allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                      links
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            output: '/rss.xml',
+            title: 'Albert Nisbet - RSS Feed',
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/pages/404.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+
+import SEO from '../components/seo'
+import Header from '../components/header'
+import Row from '../components/row'
+import Zone from '../components/zone'
+
+const NotFoundPage = ({ data }) => (
+  <div>
+    <Row>
+      <Header />
+    </Row>
+    <SEO title="404: Not Found" />
+    <Row>
+      <Zone className="markdown-body">
+        <h5>Page not found</h5>
+        <p>The page you requested does not exist.</p>
+        <Link to="/">Back to homepage</Link>
+      </Zone>
+    </Row>
+  </div>
+)
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
