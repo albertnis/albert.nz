97378b18e62be25453431fbae71aa601db10e938
Albert Nisbet <albertnis@gmail.com>
2020-07-05 15:17:46 +1200

    Use only slug as guid

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: edge.node.fields.slug,
                   custom_elements: [
                     {
                       'content:encoded': `${
