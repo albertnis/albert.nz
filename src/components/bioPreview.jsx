c3916884dedd49ebf11e1a616d8c837c75e5d3dc
Albert Nisbet <albertnis@gmail.com>
2021-08-02 21:56:09 +1200

    Tweak bio wording + remove email address

--- src/components/bioPreview.jsx
+++ src/components/bioPreview.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import * as styles from './bioPreview.module.css'
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from 'gatsby-plugin-image'
 import { useStaticQuery, graphql } from 'gatsby'
 
 const BioPreview = ({ style }) => {
-  const data = useStaticQuery(graphql`query BioQuery {
-  avatar: file(absolutePath: {regex: ""}) {
-    childImageSharp {
-      gatsbyImageData(width: 100, height: 100, layout: FIXED)
-    }
-  }
-  site {
-    siteMetadata {
-      author {
-        name
-        summary
-      }
-      social {
-        github
-        email
-        linkedin
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            github
+            email
+            linkedin
+          }
+        }
       }
     }
-  }
-}
-`)
+  `)
   const { social } = data.site.siteMetadata
   return (
     <div className={styles.biopreview} style={{ style }}>
@@ -32,24 +28,24 @@ const BioPreview = ({ style }) => {
         alt="Photo of Albert"
         className={styles.avatar}
         src="../../content/assets/profile-crop.jpg"
-        width={100}
-        height={100}
-        quality={95} 
+        width={120}
+        height={120}
+        quality={95}
         layout="fixed"
-        formats={["AUTO", "WEBP", "AVIF"]}
-        />
+        formats={['AUTO', 'WEBP', 'AVIF']}
+      />
       <div className={`markdown-body ${styles.tagline}`}>
         <h5>Kia ora!</h5>
         <p>
-          I'm Albert, a developer living and working in Christchurch. I'm into
-          engineering, home automation and electronics. Find me on{' '}
+          I'm Albert, a Christchurch-based software developer. I blog about
+          things that are interesting to me: these days that's largely home
+          automation, electronics and the outdoors. Find me on{' '}
           <a href={`https://linkedin.com/in/${social.linkedin}`}>LinkedIn</a>{' '}
-          and <a href={`https://github.com/${social.github}`}>GitHub</a>, or{' '}
-          <a href={`mailto:${social.email}`}>send an email</a>.{' '}
+          and <a href={`https://github.com/${social.github}`}>GitHub</a>.{' '}
         </p>
       </div>
     </div>
-  );
+  )
 }
 
 export default BioPreview
