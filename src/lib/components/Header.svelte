50d90970f4e17e131eda9f67ebfbe6e6f0905630
Albert Nisbet <albertnis@gmail.com>
2023-01-19 18:53:21 +1300

    Add basic layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<header>
+	<a href="/">Home</a>
+
+	<nav>
+		<ul>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
-  import "../app.css"
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
 </script>
 
+<Header />
 <slot />
