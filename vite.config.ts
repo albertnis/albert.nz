85e3d46a72ed87bacf6df999195778de825d1cfc
Albert Nisbet <albertnis@gmail.com>
2023-01-29 21:28:10 +1300

    Optimise images at build time

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 import { gpxPlugin } from './plugins/vite-plugin-gpx'
+import { imagetools } from 'vite-imagetools'
 
 const config: UserConfig = {
-	plugins: [sveltekit(), gpxPlugin({})],
+	plugins: [
+		sveltekit(),
+		gpxPlugin({}),
+		imagetools({
+			defaultDirectives: new URLSearchParams({
+				format: 'webp',
+				withoutEnlargement: 'true',
+				width: '2000'
+			})
+		})
+	],
 	server: {
 		fs: {
 			allow: ['src', 'content']
