67235ea3b69d1067045d53d6d2ceb4b0ef48dd44
Albert Nisbet <albertnis@gmail.com>
2023-02-01 08:27:57 +1300

    Style RSS link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,8 +1,12 @@
 <footer class="col-start-[wide-start] col-end-[wide-end]">
 	<div class="my-20 text-lg font-medium">
-		Copyright © 2023 <b>Albert Nisbet</b>&nbsp;&nbsp;/&nbsp;&nbsp;<a
-			data-sveltekit-reload
-			href="/rss.xml">RSS</a
+		Copyright © 2023 Albert Nisbet&nbsp<span class="text-stone-600 dark:text-stone-400"
+			>&nbsp;/&nbsp;&nbsp;<a
+				data-sveltekit-reload
+				href="/rss.xml"
+				class="border-b border-b-stone-600 hover:border-b-2 hover:border-b-blue-600 dark:border-b-stone-400 dark:hover:border-b-blue-400"
+				><b>RSS</b></a
+			></span
 		>
 	</div>
 </footer>
