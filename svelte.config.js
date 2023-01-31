cb557e27e2fb284e8dcf13fd709113510894c79a
Albert Nisbet <albertnis@gmail.com>
2023-02-01 08:28:08 +1300

    Lazy load images

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,18 @@ import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { mdsvex } from 'mdsvex'
 import relativeImages from 'mdsvex-relative-images'
+import { rehypeLazyImg } from './plugins/rehype-plugin-lazy-img/index.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-		mdsvex({ extensions: ['.md', '.svx'], remarkPlugins: [relativeImages] }),
+		mdsvex({
+			extensions: ['.md', '.svx'],
+			remarkPlugins: [relativeImages],
+			rehypePlugins: [rehypeLazyImg]
+		}),
 		vitePreprocess()
 	],
 
