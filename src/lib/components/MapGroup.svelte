5aa9f7c155109ffb25762d9b72d7819b4d9d752e
Albert Nisbet <albertnis@gmail.com>
2023-02-10 11:56:33 +1300

    Add GPX download button

--- src/lib/components/MapGroup.svelte
+++ src/lib/components/MapGroup.svelte
@@ -2,6 +2,7 @@
 	import type { ViteGpxPluginOutput } from '../../../plugins/vite-plugin-gpx/types'
 	import Map from '$lib/components/Map.svelte'
 	import ElevationGraph from '$lib/components/ElevationGraph.svelte'
+	import DownloadIcon from './DownloadIcon.svelte'
 
 	export let geo: ViteGpxPluginOutput
 	let hoveredIndex: number | undefined = undefined
@@ -13,29 +14,32 @@
 		cumulativeDistances={geo.cumulativeDistancesMetres}
 		geoJson={geo.geoJson}
 	/>
-	<a href={geo.gpxFilePath}>{geo.gpxFilePath}</a>
 	<div class="mb-5 flex justify-end align-bottom text-lg lg:text-2xl">
-		<div class="mx-3">
-			<span class="mx-1 font-bold">{(geo.distanceMetres / 1000).toFixed(1)}</span><span
+		<div class="md:mx-3">
+			<span class="font-bold sm:mx-1">{(geo.distanceMetres / 1000).toFixed(1)}</span><span
 				class="text-stone-600 dark:text-stone-400">km</span
 			>
 		</div>
 		<div class="text-stone-600 dark:text-stone-400">&nbsp;/&nbsp;</div>
-		<div class="mx-3">
-			<span class="mx-1 font-bold">{geo.cumulativeElevationGainMetres?.toLocaleString()}</span><span
-				class="text-stone-600 dark:text-stone-400">m</span
-			>
+		<div class="md:mx-3">
+			<span class="font-bold sm:mx-1">{geo.cumulativeElevationGainMetres?.toLocaleString()}</span
+			><span class="text-stone-600 dark:text-stone-400">m</span>
 		</div>
 		<div class="text-stone-600 dark:text-stone-400">&nbsp;/&nbsp;</div>
-		<div class="mx-3">
-			<span class="mx-1 font-bold">{geo.duration?.days}</span><span
+		<div class="md:mx-3">
+			<span class="font-bold sm:mx-1">{geo.duration?.days}</span><span
 				class="text-stone-600 dark:text-stone-400">d</span
-			><span class="mx-1 font-bold">{geo.duration?.hours}</span><span
+			><span class="font-bold sm:mx-1">{geo.duration?.hours}</span><span
 				class="text-stone-600 dark:text-stone-400">h</span
-			><span class="mx-1 font-bold">{geo.duration?.minutes}</span><span
+			><span class="font-bold sm:mx-1">{geo.duration?.minutes}</span><span
 				class="text-stone-600 dark:text-stone-400">m</span
 			>
 		</div>
+		<a
+			title="Download GPX file"
+			class="mx-3 flex items-center text-stone-600 hover:text-stone-800 dark:text-stone-400 dark:hover:text-stone-200"
+			href={geo.gpxFilePath}><DownloadIcon /></a
+		>
 	</div>
 	<Map {hoveredIndex} breakIndices={geo.breakIndices} geoJson={geo.geoJson} />
 </div>
