707968a6c051cd1da0079657ed16f727b9c62b1a
Albert Nisbet <albertnis@gmail.com>
2023-04-11 19:20:13 +1200

    Fix colour of map loading on light theme

--- src/lib/components/MapLoading.svelte
+++ src/lib/components/MapLoading.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="flex h-[800px] max-h-[70vh] cursor-default items-center justify-center bg-zinc-100 italic dark:bg-zinc-900"
+	class="flex h-[800px] max-h-[70vh] cursor-default items-center justify-center bg-zinc-300 italic dark:bg-zinc-900"
 >
 	<span><LoadingIcon /><span class="ml-3">Map is loading</span></span>
 </div>
