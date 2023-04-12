d500c994bab12fadc69f3e7248583e4bdced4ce2
Albert Nisbet <albertnis@gmail.com>
2023-04-12 13:29:55 +1200

    Support light mode on regular map

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -20,11 +20,23 @@
 			? geoJson.features[0].geometry.coordinates
 			: []
 
-	const darkMapStyle = 'mapbox://styles/albertnis/ckqu1h7gs6e7x17q093ii0ltr'
-
 	let mapDiv: HTMLElement | undefined
 	let map: Map
 	let hoveredMarker: MarkerType | undefined
+
+	const darkMapStyle = 'mapbox://styles/albertnis/ckqu1h7gs6e7x17q093ii0ltr'
+	const lightMapStyle = 'mapbox://styles/albertnis/ckqu3o4rn6np917qz18x1whbz'
+
+	const mapStyle = window.matchMedia('(prefers-color-scheme: dark)').matches
+		? darkMapStyle
+		: lightMapStyle
+
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+		const newMapStyle = e.matches ? darkMapStyle : lightMapStyle
+		if (map?.loaded()) {
+			map.setStyle(newMapStyle)
+		}
+	})
 
 	$: {
 		// Combining the following two if statements with `&&` breaks reactivity in prod...
@@ -49,26 +61,11 @@
 			'pk.eyJ1IjoiYWxiZXJ0bmlzIiwiYSI6ImNrcXUwNHlhcTJnODAydm84anEzanIwZHQifQ.B9-IeJHvH9nnfQT9QT4ouw'
 		map = new mapboxgl.Map({
 			container: mapDiv,
-			style: darkMapStyle,
+			style: mapStyle,
 			center: [172.82, -40.74],
 			zoom: 5
 		})
 		map.on('load', () => {
-			map.addSource('the_path', { type: 'geojson', data: geoJson })
-			map.addLayer({
-				id: 'the_path_layer',
-				type: 'line',
-				source: 'the_path',
-				layout: {
-					'line-join': 'round',
-					'line-cap': 'round'
-				},
-				paint: {
-					'line-color': '#0CF',
-					'line-width': 5
-				}
-			})
-
 			downSampledBreakIndices?.forEach((b) => {
 				const breakCoords = coords[b]
 				new Marker({ color: '#FB1' }).setLngLat([breakCoords[0], breakCoords[1]]).addTo(map)
@@ -95,6 +92,22 @@
 					padding: { top: 40, right: 40, bottom: 40, left: 40 }
 				})
 			}
+		})
+		map.on('style.load', () => {
+			map.addSource('the_path', { type: 'geojson', data: geoJson })
+			map.addLayer({
+				id: 'the_path_layer',
+				type: 'line',
+				source: 'the_path',
+				layout: {
+					'line-join': 'round',
+					'line-cap': 'round'
+				},
+				paint: {
+					'line-color': '#0CF',
+					'line-width': 5
+				}
+			})
 		})
 	})
 </script>
