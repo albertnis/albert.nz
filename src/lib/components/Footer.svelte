ea71d8f300e6729116653c87a8772bffb6962fe8
albertnis <4309984+albertnis@users.noreply.github.com>
2024-01-05 14:00:44 +1300

    Update copyright year to 2024

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="col-start-[wide-start] col-end-[wide-end]">
 	<div class="my-20 text-lg font-medium">
-		Copyright © 2023 Albert Nisbet&nbsp<span class="text-zinc-600 dark:text-zinc-400"
+		Copyright © 2024 Albert Nisbet&nbsp<span class="text-zinc-600 dark:text-zinc-400"
 			>&nbsp;/&nbsp;&nbsp;<a
 				data-sveltekit-reload
 				href="/rss.xml"
