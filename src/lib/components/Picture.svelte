77c77ecfb767f93f57c6c1039d956205e3804d06
albertnis <4309984+albertnis@users.noreply.github.com>
2024-01-28 17:50:04 +1300

    Use larger resolution for full-width images

--- src/lib/components/Picture.svelte
+++ src/lib/components/Picture.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
 	import type { Picture } from 'vite-imagetools'
-	import {} from 'svelte'
 
 	export let data: Picture | string
-	export let imgAttributes: Record<string, string>
+	export let imgAttributes: Record<string, string | undefined>
+
+	const sizes = imgAttributes?.className?.includes('prose-custom-w-full')
+		? '(max-width: 480px) 480px, (max-width: 768px) 768px, 2560px'
+		: '(max-width: 480px) 480px, 768px'
 </script>
 
-{#if typeof data === 'string'}
+{#if typeof data === 'object' && 'sources' in data}
 	<picture>
-		<img class={imgAttributes.className} {...imgAttributes} src={data} />
+		{#each Object.entries(data.sources) as [format, images]}
+			<source srcset={images} type={`image/${format}`} {sizes} />
+		{/each}
+		<img class={imgAttributes.className} {...imgAttributes} src={data.img.src} />
 	</picture>
 {:else}
 	<picture>
-		{#each Object.entries(data.sources) as [format, images]}
-			<source srcset={images} type={`image/${format}`} />
-		{/each}
-		<img class={imgAttributes.className} {...imgAttributes} src={data.img.src} />
+		<img class={imgAttributes.className} {...imgAttributes} src={data} />
 	</picture>
 {/if}
