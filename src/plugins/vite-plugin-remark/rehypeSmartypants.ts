e12b5f9434ad39098b0e00a36578eb3a699fe5e1
albertnis <4309984+albertnis@users.noreply.github.com>
2024-02-06 18:47:27 +1300

    Exclude preformatted text from smartypants

--- src/plugins/vite-plugin-remark/rehypeSmartypants.ts
+++ src/plugins/vite-plugin-remark/rehypeSmartypants.ts
@@ -1,14 +1,19 @@
-import { visit } from 'unist-util-visit'
+import { visitParents } from 'unist-util-visit-parents'
 import type { Node } from 'unist'
 import type { Literal } from 'mdast'
 import { retext } from 'retext'
 import retextSmartypants from 'retext-smartypants'
+import type { Element } from 'hast'
 
 /**
  * Remark plugin which converts grammar as defined by smartypants
  */
 export const rehypeSmartypants = () => (tree: Node) => {
-	visit(tree, { type: 'text' }, (n: Node) => {
+	visitParents(tree, { type: 'text' }, (n, ancestors: Node[]) => {
+		if (ancestors.some((a) => a.type === 'element' && (a as Element).tagName === 'pre')) {
+			// Don't touch preformatted text
+			return
+		}
 		const node = n as Literal
 		const newText = String(retext().use(retextSmartypants).processSync(node.value))
 		node.value = newText
