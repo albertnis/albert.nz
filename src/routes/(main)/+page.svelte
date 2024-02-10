aab73f30da6b8787f0a418733adb8a5fca6c5680
albertnis <4309984+albertnis@users.noreply.github.com>
2024-02-11 11:33:46 +1300

    Improve adventures view links

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -25,49 +25,48 @@
 <div class="col-start-[wide-start] col-end-[wide-end]">
 	<h2 class="text-2xl" id="posts">
 		<span class="font-bold">Latest posts</span>
-		<span class="text-zinc-600 dark:text-zinc-400">about</span>
-		<Tag tag="technology" bind:enabled={showTechnologyTag} />
-		<span class="text-zinc-600 dark:text-zinc-400">and</span>
-		<Tag tag="adventures" bind:enabled={showAdventuresTag} />
 	</h2>
 
-	<p class="mt-4 mb-8">
+	<p class="mt-4 mb-8 text-base sm:text-lg">
 		I blog about things that are interesting to me: these days that's largely home automation,
 		electronics and tramping in New Zealand.
 	</p>
 
+	<div class="my-4">
+		<span class="text-zinc-700 dark:text-zinc-300 text-base">Tags</span>
+		<ul class="inline ml-2">
+			<li class="inline text-xl"><Tag tag="technology" bind:enabled={showTechnologyTag} /></li>
+			<li class="inline text-xl"><Tag tag="adventures" bind:enabled={showAdventuresTag} /></li>
+		</ul>
+	</div>
+
 	{#if showAdventuresTag}
-		<div class="flex gap-5 my-8">
-			<div
-				class="rounded-md border border-teal-800 bg-teal-600 p-2 text-zinc-50 dark:border-teal-400 sm:w-[300px]"
-			>
-				<a class="group inline-block w-full" href="/adventures">
-					<div class="flex items-center font-bold">
-						<MapIcon />
-						<div>
-							<span class="ml-2 border-b border-b-zinc-50 group-hover:border-b-2"
-								>Adventures map</span
-							>
+		<div class="my-4 flex flex-col sm:flex-row items-baseline gap-1 sm:gap-3">
+			<span class="text-zinc-700 dark:text-zinc-300 text-base">View all adventures</span>
+			<ul class="inline text-base">
+				<li class="inline">
+					<a
+						class="group inline-block dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-950 bg-zinc-300 hover:bg-zinc-400 active:bg-zinc-50 rounded-md py-2 px-3"
+						href="/adventures"
+					>
+						<div class="flex items-center font-bold">
+							<MapIcon />
+							<span class="ml-2">Map</span>
 						</div>
-					</div>
-					<div class="text-sm text-teal-200">View adventures on an interactive map</div>
-				</a>
-			</div>
-			<div
-				class="rounded-md border border-cyan-800 bg-cyan-600 p-2 text-zinc-50 dark:border-cyan-400 sm:w-[300px]"
-			>
-				<a class="group inline-block w-full" href="/pictures">
-					<div class="flex items-center font-bold">
-						<CameraIcon />
-						<div>
-							<span class="ml-2 border-b border-b-zinc-50 group-hover:border-b-2"
-								>Adventures gallery</span
-							>
+					</a>
+				</li>
+				<li class="inline">
+					<a
+						class="group inline-block dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-950 bg-zinc-300 hover:bg-zinc-400 active:bg-zinc-50 rounded-md py-2 px-3"
+						href="/pictures"
+					>
+						<div class="flex items-center font-bold">
+							<CameraIcon />
+							<span class="ml-2">Gallery</span>
 						</div>
-					</div>
-					<div class="text-sm text-cyan-200">View gallery of adventurous photos</div>
-				</a>
-			</div>
+					</a>
+				</li>
+			</ul>
 		</div>
 	{/if}
 
