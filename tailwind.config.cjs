071731773ae7bfee6ffe6d6f948c415d62b30167
albertnis <4309984+albertnis@users.noreply.github.com>
2024-02-12 19:44:29 +1300

    Remove some unused code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,9 +11,7 @@ module.exports = {
 				'layout-md':
 					'[full-start] 1fr 1rem [image-start wide-start] minmax(0, 5rem) [prose-start] calc(768px - 2rem) [prose-end] minmax(0, 5rem) [image-end wide-end] 1rem 1fr [full-end]',
 				'layout-sm':
-					'[full-start image-start] 1rem [wide-start prose-start] 1fr [prose-end wide-end] 1rem [full-end image-end]',
-				'pictures-md': 'repeat(auto-fit, minmax(300px, 1fr))',
-				'pictures-sm': ''
+					'[full-start image-start] 1rem [wide-start prose-start] 1fr [prose-end wide-end] 1rem [full-end image-end]'
 			},
 			screens: {
 				lgxl: '1140px'
